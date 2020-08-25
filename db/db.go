1bb5030d79ff2dd98efb667e7a22d9b4ee426bcd
Mikhail Koptelov <mkoptelov@gmail.com>
2020-08-25 17:05:51 +1000

    Allowing username to be in format of user@host

--- db/db.go
+++ db/db.go
@@ -24,6 +24,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/lib/pq"
 
@@ -76,7 +77,7 @@ LC_COLLATE = 'en_US.utf8'
 LC_CTYPE = 'en_US.utf8'
 TABLESPACE = pg_default
 CONNECTION LIMIT = -1
-TEMPLATE template0;`, cfg.DbName, cfg.DbUser)
+TEMPLATE template0;`, cfg.DbName, strings.Split(cfg.DbUser, "@")[0])
 
 	fmt.Println(query)
 
